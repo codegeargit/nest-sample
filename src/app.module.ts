2be09752ec7c0dc3696de1690b04a9e336b2f1ed
codegear <dukeom@gmail.com>
2022-03-01 16:58:11 +0900

    환경설정 파일 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,16 @@ import { CatsModule } from './cats/cats.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ormConfig } from './orm.config';
+import { ConfigModule } from '@nestjs/config';
+import config from './config/config';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true
+    }),
     TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
     CatsModule,
     AuthModule
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const port = 3000;
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('server.port');
   app.use(cookieParser());
   await app.listen(port);
   console.log(`Application listening on port ${port}`);
